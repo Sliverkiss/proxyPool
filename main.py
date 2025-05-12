ec3cd563ad24b95990317e2ca4cfd7fae3930fc5
Sliverkiss <1393579810@qq.com>
2025-05-12 11:23:29 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import random
+from request_abt import get_request
+from output import http_yanzheng
+
+app = Flask(__name__)
+
+@app.route('/random', methods=['GET'])
+def random_route():
+    output_filename = "output/http.txt"
+    proxies = http_yanzheng.read_proxies_from_file(output_filename)
+    if proxies:
+        return random.choice(proxies)
+    else:
+        return 'No proxies found', 404
+
+@app.route('/verify', methods=['GET'])
+def verify_route():
+    get_request.get_http()
+    result=http_yanzheng.main()
+    return result
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=33333, debug=False)
